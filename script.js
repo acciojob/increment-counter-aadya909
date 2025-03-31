1b0cf5e7c954637b4a443c292100c9b218d6cf95
aadya909 <145979679+aadya909@users.noreply.github.com>
2025-03-31 13:49:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+let count=0;
+document.getElementById("incrementBtn").addEventListener("click",function(){
+	alert(count);
+	count++;
+	document.getElementById("counter").textContent=count;
+})
