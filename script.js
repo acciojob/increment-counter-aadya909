164ba0c54f9902a8898ec2b0dd6b99074875fe1a
aadya909 <145979679+aadya909@users.noreply.github.com>
2025-03-31 14:09:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-https://www.svgrepo.com/show/345221/three-dots.svglet count=0;
+let count=0;
 let incrementBtn = document.getElementById("incrementBtn");
 let counter = document.getElementById("counter");
 incrementBtn.addEventListener("click",function(){
